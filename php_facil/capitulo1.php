35b5b65ee5d498abdac7bc560f6d1e3137551226
mestanza <carlos_j_mestanza@hotmail.com>
2018-07-22 19:56:32 -0500

    Add files via upload

---
+++ php_facil/capitulo1.php
@@ -0,0 +1,42 @@
+<html>
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+	<link rel="stylesheet" type="text/css" href="style.css">
+	<link href="https://fonts.googleapis.com/css?family=Roboto+Condensed" rel="stylesheet">
+</head>
+<body>
+	<h1>PHP Fácil</h1>
+	<p>Hola</p>
+	<p>Yo soy Carlos Mestanza y a continuación te mostraré los primeros pasos para usar PHP de la manera más sencilla</p>
+	<p>Lo principal que se necesita para ejecutar PHP es tener instalado un servidor en nuestro equipo local. Sin la instalacion de algun servidor, nuestro codigo se vuelve inservible</p>
+	<p>Para este caso, recomiendo usar XAMMP. El cual es un paquete de software libre, que consiste principalmente en el sistema de gestión de bases de datos MySQL e intérprete de lenguajes con PHP</p>
+	<p>Pasos para descargar e instalar XAMMP:</p>
+	<ol>
+		<li>Ingresar en el siguiente Link: <a href="https://www.apachefriends.org/es/index.html">https://www.apachefriends.org/es/index.html</a></li>
+		<li>Seleccionar el tipo de descarga, para este caso usaremos Windows<br>
+		<img class="imgXammp" src="img/xammp1.JPG" alt="xammp1">
+		</li>
+		<li>Una vez realizada la descarga se ejecuta como administrador<br>
+		<img class="imgXammp2" src="img/xammp2.JPG" alt="xammp2">
+		</li>
+		<li>Se realiza una instalacion sencilla pulsando "Next", tal cual como indican las siguientes imagenes<br>
+		<div class="imgInline">
+			<img class="imgXammp" src="img/xammp3.JPG" alt="xammp2">
+			<img class="imgXammp" src="img/xammp4.JPG" alt="xammp2">
+			<img class="imgXammp" src="img/xammp5.JPG" alt="xammp2">
+		</div>
+		<div class="imgInline">
+			<img class="imgXammp" src="img/xammp6.JPG" alt="xammp2">
+			<img class="imgXammp" src="img/xammp7.JPG" alt="xammp2">
+		</div>	
+		</li>
+		<p>Luego se procede a ejecutar el servidor local (localhost), desde el navegador de su preferencia. Esto se logra escribiendo "localhost" en la barra de navegacion (sin las comillas).</p><br>
+		<img class="imgXammp" src="img/xammp8.JPG" alt="xammp2">
+		<p>Si se ve la imagen anterior en tu navegador, significa que todo se realizó de manera exitosa. En caso contrario, revisa los pasos de instalación</p>
+		<p>En caso de instalación exitosa, pulsa en la esquina superior derecha, donde dice "phpMyAdmin". Este será el gestor de base de datos.</p>
+		<p>Este es el primer paso a ejecutar para conocer el mundo de PHP. Pronto continuaré subiendo mas tutoriales.</p>
+		<p>Nos vemos</p>
+	</ol>
+</body>
+</html>
